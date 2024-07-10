8d5eb718ddfd1a3b85d74abc3b9e89132be63bad
Onji Kim <xodks672@gmail.com>
2024-07-10 11:56:34 +0900

    init : base structure

---
+++ src/main/java/org/likelion/kkokio/KkokioApplication.java
@@ -0,0 +1,13 @@
+package org.likelion.kkokio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KkokioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KkokioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/likelion/kkokio/KkokioApplicationTests.java
@@ -0,0 +1,13 @@
+package org.likelion.kkokio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KkokioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
