b1902003ee6615267a46f608ce3290b5d70c6d05
Onji Kim <xodks672@gmail.com>
2024-07-30 10:15:22 +0900

    feat : 회원가입 기능 추가

---
+++ src/main/java/org/likelion/kkokio/domain/adminAccount/controller/AccountController.java
@@ -0,0 +1,25 @@
+package org.likelion.kkokio.domain.adminAccount.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.likelion.kkokio.domain.adminAccount.dto.AccountRegisterDto;
+import org.likelion.kkokio.domain.adminAccount.dto.AccountView;
+import org.likelion.kkokio.domain.adminAccount.service.AccountService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class AccountController {
+    private final AccountService accountService;
+
+    @PostMapping("/accounts")
+    @ResponseStatus(HttpStatus.CREATED)
+    public AccountView register(
+            @Valid AccountRegisterDto registerDto
+    ) {
+        return accountService.registerAccount(registerDto);
+    }
+}
