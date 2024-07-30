6fbbaa3e619fdef17bce238a259eabb244cd7ebc
fdoom <11jjklol@naver.com>
2024-07-30 16:37:19 +0900

    fix: account 관련 인증 허용 처리

--- src/main/java/org/likelion/kkokio/domain/security/config/SecurityConfig.java
+++ src/main/java/org/likelion/kkokio/domain/security/config/SecurityConfig.java
@@ -51,7 +51,7 @@ public class SecurityConfig {
                 )
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests((authorize) -> authorize
-                        .requestMatchers(HttpMethod.POST, "/account").permitAll()
+                        .requestMatchers("/accounts/**").permitAll()
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .requestMatchers("/login").permitAll()
                         .requestMatchers("/store/info").authenticated()
